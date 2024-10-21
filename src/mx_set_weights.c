dbb2cfefb228226a483c809817cce4ff3d3429ee
EvgeniaRezchik <144226772+EvgeniaRezchik@users.noreply.github.com>
2024-10-21 21:45:14 +0300

    Update mx_set_weights.c

--- src/mx_set_weights.c
+++ src/mx_set_weights.c
@@ -1,20 +1,19 @@
 #include "../libmx/inc/libmx.h"
 #include "../inc/pathfinder.h"
 
-void mx_set_weights(t_island **il, t_path *p, int ln, int count, char *dpr) {
+void mx_set_weights(t_island **il, t_path *p, int ln, int count, int dpr) {
     int c = 0;
     bool set = true;
 
-    (*il)[mx_index(dpr, *il, count)].usage = c;
+    (*il)[dpr].usage = c;
     while (set) {
         set = false;
 	for (int i = 0; i < count; i += 1) {
 	    if ((*il)[i].usage == c) {
 	        for (int j = 0; j < ln; j += 1)
-		    mx_set_bool(il, p, dpr, i, j, c, count, &set);
+		    mx_set_bool(il, p, dpr, i, j, c, &set);
 	    }
 	}
 	c += 1;
     }
 }
-
