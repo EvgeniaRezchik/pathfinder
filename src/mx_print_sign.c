70212afc4530189bd1503dc8f8b29f0a1838c688
EvgeniaRezchik <144226772+EvgeniaRezchik@users.noreply.github.com>
2024-10-21 21:53:50 +0300

    Update mx_print_sign.c

--- src/mx_print_sign.c
+++ src/mx_print_sign.c
@@ -1,27 +1,21 @@
 #include "../libmx/inc/libmx.h"
 #include "../inc/pathfinder.h"
 
-int mx_print_sign(t_island *il, t_path *p, char *d,
-		  char *ds, int c, int *cu, int i) {
-    if ((mx_strcmp(il[*cu].name, p[i].first) == 0
-	&& mx_strcmp(p[i].second, ds) == 0)
-	|| (mx_strcmp(il[*cu].name, p[i].second) == 0
-	&& mx_strcmp(p[i].first, ds) == 0)) {
-        if (mx_strcmp(p[i].first, d) != 0
-	    && mx_strcmp(p[i].second, d) != 0) {
+int mx_print_sign(t_island *il, t_path *p, int d,
+		  int ds, int *cu, int i) {
+    if ((il[*cu].id == p[i].id1 && p[i].id2 == ds)
+	|| (il[*cu].id == p[i].id2 && p[i].id1 == ds)) {
+        if (p[i].id1 != d && p[i].id2 != d) {
 	    mx_printint(p[i].length);
 	    mx_printstr(" = ");
 	}
-	mx_printint(il[mx_index(ds, il, c)].weight);
+	mx_printint(il[ds].weight);
 	return 1;
     }
     else {
         mx_printint(p[i].length);
 	mx_printstr(" + ");
-	*cu = mx_strcmp(il[*cu].name, p[i].first) == 0 ?
-	    mx_index(p[i].second, il, c) :
-	    mx_index(p[i].first, il, c);
+	*cu = il[*cu].id == p[i].id1 ? p[i].id2 : p[i].id1;
 	return 0;
     }
 }
-
