eb70240a4021ca65c71142ae504b82812a4ecc3d
EvgeniaRezchik <144226772+EvgeniaRezchik@users.noreply.github.com>
2024-10-21 21:43:57 +0300

    Update pathfinder.h

--- inc/pathfinder.h
+++ inc/pathfinder.h
@@ -5,13 +5,16 @@
 
 typedef struct s_island {
     char *name;
+    int id;
     int usage;
     unsigned int weight;
 }              t_island;
 
 typedef struct s_path {
     char *first;
+    int id1;
     char *second;
+    int id2;
     unsigned int length;
 }              t_path;
 
@@ -37,26 +40,24 @@ void mx_del_paths(t_path **paths, int line);
 void mx_del_islands(t_island **islands, int count);
 void mx_delfirst_printerr5(int line, char **s, int file, t_path **paths);
 void mx_delislands_printerr5(int line, char **s, int file, t_path **paths);
-void mx_fill_islands(t_island **islands, int *count, t_path *paths, int ln);
-void mx_add_island(t_island **islands, int *count, char *str);
+void mx_fill_islands(t_island **islands, int *count, t_path **paths, int ln);
+void mx_add_first(t_island **islands, int *count, t_path **paths, int i);
+void mx_add_second(t_island **islands, int *count, t_path **paths, int i);
 bool mx_pathscmp(t_path *paths, int line);
 void mx_find_paths(t_island **islands, t_path *paths, int line, int count);
-int mx_index(char *str, t_island *islands, int count);
-void mx_set_weights(t_island **il, t_path *p, int ln, int count, char *dpr);
-void mx_set_bool(t_island **il, t_path *p, char *d, int i,
-		 int j, int c, int co, bool *s);
+void mx_set_weights(t_island **il, t_path *p, int ln, int count, int dpr);
+void mx_set_bool(t_island **il, t_path *p, int d, int i,
+		 int j, int c, bool *s);
 void mx_build_paths(t_island **il, t_path *p, int l,
-		    int c, char *d, char *ds, char *s);
+		    int c, int d, int ds, int s, int i);
 void mx_pave_ways(t_island **il, t_path *p, int l, int c,
-		  char *d, char *ds, char *s, int i);
-void mx_print_block(t_island *i, t_path *p, int l, int c, char *d, char *ds);
-void mx_print_route(t_island *il, t_path *p, int l, int c, char *d, char *ds);
-int mx_print_way(t_island *il, t_path *p, char *ds, int c, int *curr, int i);
-void mx_print_dist(t_island *il, t_path *p, int l, int c, char *d, char *ds);
-int mx_print_sign(t_island *il, t_path *p, char *d,
-		  char *ds, int c, int *cu, int i);
+		  int d, int ds, int s, int i, int j, int id);
+void mx_print_block(t_island *i, t_path *p, int l, int d, int ds);
+void mx_print_route(t_island *il, t_path *p, int l, int d, int ds);
+int mx_print_way(t_island *il, t_path *p, int ds, int *curr, int i);
+void mx_print_dist(t_island *il, t_path *p, int l, int d, int ds);
+int mx_print_sign(t_island *il, t_path *p, int d, int ds, int *cu, int i);
 void mx_reset_usage(t_island **islands, int count);
 void mx_nullify_weight(t_island **islands, int count);
 
 #endif
-
