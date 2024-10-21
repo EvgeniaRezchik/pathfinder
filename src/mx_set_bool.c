526e9c5a5d63c04c55382aa6291c6750daa5046a
EvgeniaRezchik <144226772+EvgeniaRezchik@users.noreply.github.com>
2024-10-21 21:45:48 +0300

    Update mx_set_bool.c

--- src/mx_set_bool.c
+++ src/mx_set_bool.c
@@ -1,23 +1,20 @@
 #include "../libmx/inc/libmx.h"
 #include "../inc/pathfinder.h"
 
-void mx_set_bool(t_island **il, t_path *p, char *d, int i,
-		 int j, int c, int co, bool *s) {
-    if (mx_strcmp((*il)[i].name, p[j].first) == 0
-	&& (((*il)[mx_index(p[j].second, *il, co)].weight == 0
-	&& mx_strcmp((*il)[mx_index(p[j].second, *il, co)].name, d) != 0)
-	|| (*il)[mx_index(p[j].second, *il, co)].weight > (*il)[i].weight + p[j].length)) {
-        (*il)[mx_index(p[j].second, *il, co)].weight = (*il)[i].weight + p[j].length;
-	(*il)[mx_index(p[j].second, *il, co)].usage = c + 1;
+void mx_set_bool(t_island **il, t_path *p, int d, int i,
+		 int j, int c, bool *s) {
+    if ((*il)[i].id == p[j].id1 && (((*il)[p[j].id2].weight == 0
+	&& (*il)[p[j].id2].id != d)
+	|| (*il)[p[j].id2].weight > (*il)[i].weight + p[j].length)) {
+        (*il)[p[j].id2].weight = (*il)[i].weight + p[j].length;
+	(*il)[p[j].id2].usage = c + 1;
 	*s = true;
     }
-    else if (mx_strcmp((*il)[i].name, p[j].second) == 0
-	     && (((*il)[mx_index(p[j].first, *il, co)].weight == 0
-	     && mx_strcmp((*il)[mx_index(p[j].first, *il, co)].name, d) != 0)
-	     || (*il)[mx_index(p[j].first, *il, co)].weight > (*il)[i].weight + p[j].length)) {
-        (*il)[mx_index(p[j].first, *il, co)].weight = (*il)[i].weight + p[j].length;
-	(*il)[mx_index(p[j].first, *il, co)].usage = c + 1;
+    else if ((*il)[i].id == p[j].id2 && (((*il)[p[j].id1].weight == 0
+	     && (*il)[p[j].id1].id != d)
+	     || (*il)[p[j].id1].weight > (*il)[i].weight + p[j].length)) {
+        (*il)[p[j].id1].weight = (*il)[i].weight + p[j].length;
+	(*il)[p[j].id1].usage = c + 1;
 	*s = true;
     }
 }
-
