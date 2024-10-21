a89f57f8a07d9a63bf5f50237830460940be3cc0
EvgeniaRezchik <144226772+EvgeniaRezchik@users.noreply.github.com>
2024-10-21 21:54:42 +0300

    Update mx_read_first_name.c

--- src/mx_read_first_name.c
+++ src/mx_read_first_name.c
@@ -15,8 +15,9 @@ int mx_read_first_name(int line, char **s, int file, t_path **paths) {
         mx_error5(line, s, file, paths);
     *paths = mx_realloc(*paths, (line - 1) * sizeof(t_path));
     (*paths)[line - 2].first = mx_strdup(*s);
+    (*paths)[line - 2].id1 = -1;
     (*paths)[line - 2].second = NULL;
+    (*paths)[line - 2].id2 = -1;
     mx_strdel(s);
     return c;
 }
-
