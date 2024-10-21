c1b1b389ff48add859685876c9685d590014ac73
EvgeniaRezchik <144226772+EvgeniaRezchik@users.noreply.github.com>
2024-10-21 21:52:25 +0300

    Update mx_print_block.c

--- src/mx_print_block.c
+++ src/mx_print_block.c
@@ -1,15 +1,14 @@
 #include "../libmx/inc/libmx.h"
 #include "../inc/pathfinder.h"
 
-void mx_print_block(t_island *i, t_path *p, int l, int c, char *d, char *ds) {
+void mx_print_block(t_island *i, t_path *p, int l, int d, int ds) {
     mx_printstr("========================================\nPath: ");
-    mx_printstr(i[mx_index(d, i, c)].name);
+    mx_printstr(i[d].name);
     mx_printstr(" -> ");
-    mx_printstr(i[mx_index(ds, i, c)].name);
+    mx_printstr(i[ds].name);
     mx_printstr("\nRoute: ");
-    mx_print_route(i, p, l, c, d, ds);
+    mx_print_route(i, p, l, d, ds);
     mx_printstr("\nDistance: ");
-    mx_print_dist(i, p, l, c, d, ds);
+    mx_print_dist(i, p, l, d, ds);
     mx_printstr("\n========================================\n");
 }
-
